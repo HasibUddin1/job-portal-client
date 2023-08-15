180c3e17a3129d8a3b6f73d85e68751caf053cc6
IconicLuck <uhasib959@gmail.com>
2023-08-15 12:18:07 +0600

    added contact us and about us page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
             <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
                 <div className="md:flex md:justify-between">
                     <div className="mb-6 md:mb-0">
-                        <a href="https://flowbite.com/" className="flex items-center">
+                        <a href="#" className="flex items-center">
                             <img src={logoImage} className="h-8 mr-3" alt="FlowBite Logo" />
                             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Easy Job</span>
                         </a>
@@ -17,10 +17,10 @@ const Footer = () => {
                             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
                             <ul className="text-gray-500 dark:text-gray-400 font-medium">
                                 <li className="mb-4">
-                                    <a href="https://flowbite.com/" className="hover:underline">Easy Job</a>
+                                    <a href="#" className="hover:underline">Easy Job</a>
                                 </li>
                                 <li>
-                                    <a href="https://tailwindcss.com/" className="hover:underline">Location</a>
+                                    <a href="#" className="hover:underline">Location</a>
                                 </li>
                             </ul>
                         </div>
@@ -28,10 +28,10 @@ const Footer = () => {
                             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
                             <ul className="text-gray-500 dark:text-gray-400 font-medium">
                                 <li className="mb-4">
-                                    <a href="https://github.com/themesberg/flowbite" className="hover:underline ">Github</a>
+                                    <a href="#" className="hover:underline ">Github</a>
                                 </li>
                                 <li>
-                                    <a href="https://discord.gg/4eeurUVvTy" className="hover:underline">Discord</a>
+                                    <a href="#" className="hover:underline">Discord</a>
                                 </li>
                             </ul>
                         </div>
@@ -50,7 +50,7 @@ const Footer = () => {
                 </div>
                 <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
                 <div className="sm:flex sm:items-center sm:justify-between">
-                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">Easy Job</a>. All Rights Reserved.
+                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="#" className="hover:underline">Easy Job</a>. All Rights Reserved.
                     </span>
                     <div className="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
                         <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -1,11 +1,61 @@
+import { FaLocationArrow, FaUser, FaWhatsapp } from "react-icons/fa";
+import { MdOutgoingMail } from "react-icons/md";
+import contactImage from '../../assets/images/contact-section-image.jpg'
 
 
-const ContactUs = () => {
+const Contact = () => {
     return (
-        <div className="min-h-screen">
-            <h1 className="text-center text-4xl">This is Contact Us Component</h1>
+        <div className='w-9/12 mx-auto mt-20 mb-20 min-h-screen'>
+            <h4 className="text-2xl text-center font-semibold mb-2">Get in Touch</h4>
+            <h1 className="text-4xl text-center font-bold mb-10">Our Contact Details</h1>
+            <div className='md:flex items-center mb-20'>
+                <div className='md:w-1/2 mb-10 md:mb-0'>
+                    <img className='rounded-xl' src={contactImage} alt="" />
+                </div>
+                <div className='md:w-1/2'>
+                    <h2 className='text-rose-600 font-bold text-2xl mb-2'>Message Us</h2>
+                    <p className='text-xl mb-5'>If you have any offer, queries, suggestions, opinion, objection - feel free to let me know through email, phone or any other social media platform that I have shared in my portfolio.</p>
+                    <div className='flex items-center gap-5 mb-5'>
+                        <div>
+                            <FaUser className='text-4xl text-rose-600'></FaUser>
+                        </div>
+                        <div>
+                            <p className='font-bold'>Name</p>
+                            <p>Easy Job</p>
+                        </div>
+                    </div>
+                    <div className='flex items-center gap-5 mb-5'>
+                        <div>
+                            <FaLocationArrow className='text-4xl text-rose-600'></FaLocationArrow>
+                        </div>
+                        <div>
+                            <p className='font-bold'>Location</p>
+                            <p>Mirpur, Dhaka -1216</p>
+                        </div>
+                    </div>
+                    <div className='flex items-center gap-5 mb-5'>
+                        <div>
+                            <FaWhatsapp className='text-4xl text-rose-600'></FaWhatsapp>
+                        </div>
+                        <div>
+                            <p className='font-bold'>Whatsapp</p>
+                            <p>+8801782149686</p>
+                        </div>
+                    </div>
+                    <div className='flex items-center gap-5 mb-5'>
+                        <div>
+                            <MdOutgoingMail className='text-4xl text-rose-600'></MdOutgoingMail>
+                        </div>
+                        <div>
+                            <p className='font-bold'>Email</p>
+                            <p>easyjob@gmail.com</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 };
 
-export default ContactUs;
+export default Contact;
